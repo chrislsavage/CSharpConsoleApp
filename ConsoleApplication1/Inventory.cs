974b9131c9f1fc535ff74c303c23187b1b9e90a0
c <c@DESKTOP-COL5Q3R>
2016-05-03 14:14:15 -0400

    Add project files.

---
+++ ConsoleApplication1/Inventory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Inventory
+    {
+        List<Product> items = new List<Product>
+        {
+            new Product() {Name= "Container", Price = 0 }
+        };
+
+        public void AddItem(Product item)
+        {
+            items.Add(item);
+        }
+
+        public void DeleteItem(Product item)
+        {
+            items.Remove(item);
+        }
+
+        public void ListItems()
+        {
+            foreach(var item in this.items)
+            {
+                Console.WriteLine($"Name: {item.Name}, Price: {item.Price}");
+            }
+        }
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public interface Consoler
+        // interfaces can only contain function declarations
+    {
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product bag = new Product();
+            Console.WriteLine($"The Product's name is {bag.name}, and my price is {bag.price}" );
+        }
+    }
+}
+
